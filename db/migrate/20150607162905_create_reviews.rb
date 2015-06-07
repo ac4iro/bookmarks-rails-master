5bd04cff4fbb5d5d889c6e0ac7000ad92e37a1f0
anabel cairo <YOUR@EMAIL.com>
2015-06-07 16:18:20 -0400

    gemfile

--- db/migrate/20150607162905_create_reviews.rb
+++ db/migrate/20150607162905_create_reviews.rb
@@ -1,7 +1,9 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-
+    	t.string :author
+    	t.string :comment
+    	t.references :book, index: true
       t.timestamps
     end
   end
